7beb05c493e37f35b953cac0f22a13228cd5685e
jyodai <tdkdtk5011@gmail.com>
2023-07-17 03:39:56 +0900

    * コード規約

--- problemList/addTwoNumbers.php
+++ problemList/addTwoNumbers.php
@@ -2,23 +2,28 @@
 
 /**
  * Definition for a singly-linked list.
- * class ListNode {
- *     public $val = 0;
- *     public $next = null;
- *     function __construct($val = 0, $next = null) {
- *         $this->val = $val;
- *         $this->next = $next;
- *     }
- * }
  */
-class Solution {
+class ListNode
+{
+    public $val  = 0;
+    public $next = null;
+    public function __construct($val = 0, $next = null)
+    {
+        $this->val  = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
 
     /**
      * @param ListNode $l1
      * @param ListNode $l2
      * @return ListNode
      */
-    function addTwoNumbers($l1, $l2) {
+    public function addTwoNumbers($l1, $l2)
+    {
         $reverse1 = $this->convertArray($l1);
         $reverse2 = $this->convertArray($l2);
         
@@ -28,9 +33,9 @@ class Solution {
             $count = count($reverse1);
         }
         
-        $sumArray = [];
+        $sumArray         = [];
         $carryForwardFlag = false;
-        for($i = 0; $i < $count; $i++) {
+        for ($i = 0; $i < $count; $i++) {
             $num1 = isset($reverse1[$i]) ? $reverse1[$i] : 0;
             $num2 = isset($reverse2[$i]) ? $reverse2[$i] : 0;
 
@@ -56,7 +61,8 @@ class Solution {
         return $listNode;
     }
 
-    function convertArray($l) {
+    private function convertArray($l)
+    {
         $ret = [];
         array_push($ret, $l->val);
         if ($l->next !== null) {
@@ -65,10 +71,11 @@ class Solution {
         return $ret;
     }
 
-    function convertListNode($array) {
+    private function convertListNode($array)
+    {
         $reverseArray = array_reverse($array);
-        $listNode = null;
-        foreach($reverseArray as $val) {
+        $listNode     = null;
+        foreach ($reverseArray as $val) {
             $listNode = new ListNode($val, $listNode);
         }
         return $listNode;
--- problemList/twoSum.php
+++ problemList/twoSum.php
@@ -1,21 +1,57 @@
 <?php
 
-class Solution {
+class Solution
+{
 
   /**
-   * @param Integer[] $nums
-   * @param Integer $target
-   * @return Integer[]
+   * @param int[] $nums
+   * @param int $target
+   * @return int[]
    */
-  function twoSum($nums, $target) {
-      $ret = [];
+    public function twoSum($nums, $target)
+    {
+        $ret = [];
 
-      foreach ($nums as $index => $numA) {
-          $searchNum = $target - $numA;
-          $searchIndex = array_search($searchNum, $nums);
-          if ($searchIndex !== false && $index !== $searchIndex) {
-              return [$index, $searchIndex];
-          }
-      }
-  }
+        foreach ($nums as $index => $numA) {
+            $searchNum   = $target - $numA;
+            $searchIndex = array_search($searchNum, $nums);
+            if ($searchIndex !== false && $index !== $searchIndex) {
+                return [$index, $searchIndex];
+            }
+        }
+    }
+
+    public function execTest()
+    {
+        $this->testFirst();
+        $this->testSecond();
+        $this->testThird();
+    }
+
+    public function testFirst()
+    {
+        $nums   = [2,7,11,15];
+        $target = 9;
+        $result = $this->twoSum($nums, $target);
+        assert($result === [0, 1] || $result === [1, 0]);
+    }
+  
+    public function testSecond()
+    {
+        $nums   = [3,2,4];
+        $target = 6;
+        $result = $this->twoSum($nums, $target);
+        assert($result === [1, 2] || $result === [2, 1]);
+    }
+
+    public function testThird()
+    {
+        $nums   = [3,3];
+        $target = 6;
+        $result = $this->twoSum($nums, $target);
+        assert($result === [0, 1] || $result === [1, 0]);
+    }
 }
+
+$solution = new Solution();
+$solution->execTest();
