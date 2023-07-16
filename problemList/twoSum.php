5e441bafae930049da6e1ea47b26ae919bf591e5
jyodai <50922604+jyodai@users.noreply.github.com>
2023-07-16 18:50:35 +0900

    Create twoSum.php

---
+++ problemList/twoSum.php
@@ -0,0 +1,19 @@
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @param Integer $target
+   * @return Integer[]
+   */
+  function twoSum($nums, $target) {
+      $ret = [];
+
+      foreach ($nums as $index => $numA) {
+          $searchNum = $target - $numA;
+          $searchIndex = array_search($searchNum, $nums);
+          if ($searchIndex !== false && $index !== $searchIndex) {
+              return [$index, $searchIndex];
+          }
+      }
+  }
+}
