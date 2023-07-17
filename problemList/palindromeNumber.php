cb29925de50dd17f32c749129ab1d10f23305923
jyodai <tdkdtk5011@gmail.com>
2023-07-18 08:19:51 +0900

    文字列を反転させる標準関数が存在した

--- problemList/palindromeNumber.php
+++ problemList/palindromeNumber.php
@@ -10,17 +10,9 @@ class Solution
     public function isPalindrome($x)
     {
         if ($x < 0) {
-            false;
+            return false;
         }
         $str = (string) $x;
-
-        $count = strlen($str);
-        for ($i = 0; $i < $count; $i++) {
-            if (substr($str, $i, 1) !== substr($str, $count - $i - 1, 1)) {
-                return false;
-            }
-        }
-
-        return true;
+        return strrev($str) === $str;
     }
 }
