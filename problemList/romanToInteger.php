f7bdfd636e164736d4456ceacaf124e2b4c28047
jyodai <tdkdtk5011@gmail.com>
2023-07-18 09:31:31 +0900

    ループを減らす

--- problemList/romanToInteger.php
+++ problemList/romanToInteger.php
@@ -9,50 +9,44 @@ class Solution
      */
     public function romanToInt($s)
     {
-        $numArray = [];
-        $array    = str_split($s);
+        $num   = 0;
+        $array = str_split($s);
 
-        $skip = false;
         for ($i = 0; $i < count($array); $i++) {
             $next = $i + 1;
-
-            if ($array[$i] === 'I' && ($array[$next] === 'V' || $array[$next] === 'X')) {
-                array_push($numArray, -1);
-                continue;
+            if ($array[$next]) {
+                $convertNum = $this->convert($array[$i], $array[$next]);
+            } else {
+                $convertNum = $this->convert($array[$i]);
             }
-
-            if ($array[$i] === 'X' && ($array[$next] === 'L' || $array[$next] === 'C')) {
-                array_push($numArray, -10);
-                continue;
-            }
-
-            if ($array[$i] === 'C' && ($array[$next] === 'D' || $array[$next] === 'M')) {
-                array_push($numArray, -100);
-                continue;
-            }
-
-            array_push($numArray, $this->convert($array[$i]));
-        }
-
-        $num = 0;
-        for ($i = 0; $i < count($numArray); $i++) {
-            $num = $num + $numArray[$i];
+            $num = $num + $convertNum;
         }
 
         return $num;
     }
 
-    private function convert($str)
+    private function convert($strA, $strB = '')
     {
         $roman = [
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
+            'I'  => 1,
+            'V'  => 5,
+            'X'  => 10,
+            'L'  => 50,
+            'C'  => 100,
+            'D'  => 500,
+            'M'  => 1000,
+            'IV' => -1,
+            'IX' => -1,
+            'XL' => -10,
+            'XC' => -10,
+            'CD' => -100,
+            'CM' => -100,
         ];
-        return $roman[$str];
+
+        if (array_key_exists($strA . $strB, $roman)) {
+            return $roman[$strA . $strB];
+        } else {
+            return $roman[$strA];
+        }
     }
 }
