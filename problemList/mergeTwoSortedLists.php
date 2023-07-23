cc3a4810fdbbde509a5ad2ae020109f69f01811b
jyodai <tdkdtk5011@gmail.com>
2023-07-23 13:21:28 +0900

    Create mergeTwoSortedLists.php

---
+++ problemList/mergeTwoSortedLists.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    public function mergeTwoLists($list1, $list2)
+    {
+        $array1 = $this->convertArray($list1);
+        $array2 = $this->convertArray($list2);
+
+        $merge = array_merge($array1, $array2);
+        asort($merge);
+
+        return $this->convertList($merge);
+    }
+
+    public function convertArray($list): array
+    {
+        if ($list->val === null) {
+            return [];
+        }
+
+        $ret    = [];
+        $target = null;
+        do {
+            if ($target === null) {
+                $target = $list;
+            } else {
+                $target = $target->next;
+            }
+            array_push($ret, $target->val);
+        } while ($target->next !== null);
+        return $ret;
+    }
+
+    public function convertList($merge)
+    {
+        $val = array_shift($merge);
+        if (count($merge) === 0) {
+            return new ListNode($val);
+        } else {
+            return new ListNode($val, $this->convertList($merge));
+        }
+    }
+}
