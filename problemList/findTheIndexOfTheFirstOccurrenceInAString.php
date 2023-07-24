0c909655c05add32467a07e79a60cdfe7abe16f0
jyodai <tdkdtk5011@gmail.com>
2023-07-24 23:42:43 +0900

    Create findTheIndexOfTheFirstOccurrenceInAString.php

---
+++ problemList/findTheIndexOfTheFirstOccurrenceInAString.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $haystack
+     * @param String $needle
+     * @return Integer
+     */
+    function strStr($haystack, $needle) {
+        $ret = strpos($haystack, $needle);
+        return $ret === false ? -1 : $ret;
+    }
+}
