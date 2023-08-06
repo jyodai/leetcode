cd982ab9dfb8b61e980f7f56671d74aa0593c762
jyodai <tdkdtk5011@gmail.com>
2023-08-06 22:39:35 +0900

    Create reverseString.php

---
+++ problemList/reverseString.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param String[] $s
+     * @return NULL
+     */
+    public function reverseString(&$s)
+    {
+        $s = array_reverse($s);
+    }
+}
